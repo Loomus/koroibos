d1a1c824821c9483f0b30454d93367501a291e1a
Andrew Johnson <manilda7@gmail.com>
2020-01-14 17:45:17 -0700

    Adds work to olympians route

--- app/routes/api/v1/olympians.js
+++ app/routes/api/v1/olympians.js
@@ -3,14 +3,28 @@ const router = express.Router()
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../../knexfile')[environment]
 const database = require('knex')(configuration)
+const Olympian = require('../../../models/olympian.js')
 
-router.get('/', async (request, response) => {
-  const olympians = await database('olympians').then(result => result)
-  if (olympians) {
-    response.status(200).json(olympians)
+
+const getOlympians = router.get('/', async (request, response) => {
+  if (request.query.age === 'oldest') {
+    newQuery = 'ORDER BY olympian.age DESC LIMIT 1'
+  } else if (request.query.age === 'youngest') {
+    newQuery = 'ORDER BY olympian.age ASC LIMIT 1'
   } else {
-    response.status(404).json('No olympians in database')
+    newQuery = ' '
   }
+  const olympians = await database('olympians').then(result => result)
+  if (request.query.age === 'youngest' || request.query.age === 'oldest') {
+    response.status(200).json(olympians.getByAge, newQuery);
+  } else if () {
+    response.status(200).json(olympians)
+    // response.status(200).send({'olympians': olympians['rows']})
+    // response.status(404).json('No olympians in database')
+  } else {
+
+  }
+
 })
 
-module.exports = router
+module.exports = getOlympians
