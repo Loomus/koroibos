d8023b772c22b36a3eb104a37ae0acd09b033c03
Andrew Johnson <manilda7@gmail.com>
2020-01-14 18:47:31 -0700

    Removes Olympian model

--- app/models/olympian.js
+++ app/models/olympian.js
@@ -1,13 +1,13 @@
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../knexfile')[environment];
-const database = require('knex')(configuration);
-
-
-const getByAge = (age) => database('olympians')
-	.select()
-	.orderBy('age', age)
-	.limit(1)
-	.then(total => total)
-	.catch(error => error)
-
-  module.exports = getByAge
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('../../knexfile')[environment];
+// const database = require('knex')(configuration);
+//
+//
+// const getByAge = (age) => database('olympians')
+// 	.select()
+// 	.orderBy('age', age)
+// 	.limit(1)
+// 	.then(total => total)
+// 	.catch(error => error)
+//
+// module.exports = getByAge
