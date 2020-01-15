431b19ad56758f244ee5190382ebe460755baa5a
Andrew Johnson <manilda7@gmail.com>
2020-01-14 22:08:09 -0700

    Adds olympian stats route

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 
-var indexRouter = require('./app/routes/index.js');
-var olympiansRouter = require('./app/routes/api/v1/olympians.js');
+var indexRouter = require('./app/routes/index.js')
+var olympiansRouter = require('./app/routes/api/v1/olympians.js')
+var olympianStatsRouter = require('./app/routes/api/v1/olympian_stats.js')
 
 
 var app = express();
@@ -18,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api/v1/olympians', olympiansRouter);
+app.use('/', indexRouter)
+app.use('/api/v1/olympians', olympiansRouter)
+app.use('/api/v1/olympian_stats', olympianStatsRouter)
 
 module.exports = app;
